8135dbdfc93be1c3d7fd921ccae5d13abaa8dc0c
MoonHongMyeong <moonhongmyeong@gmail.com>
2023-11-21 16:55:54 +0900

    Add : Mybatis와 DB연결을 위한 설정파일 작성 [#1]

---
+++ src/main/java/me/study/boilerplate/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package me.study.boilerplate.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan({"me.study.boilerplate"})
+public class DatabaseConfig {
+    private final ApplicationContext applicationContext;
+
+    public DatabaseConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SqlSessionFactory sessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setMapperLocations(applicationContext.getResource("classpath:/mapper/**/*.xml"));
+
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sessionTemplate(SqlSessionFactory sessionFactory){
+        return new SqlSessionTemplate(sessionFactory);
+    }
+}
